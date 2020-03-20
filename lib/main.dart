858397a919341a7fd0a4c51bdd05bf20e721f184
Phil Sun <psun.04@gmail.com>
2020-03-19 23:11:47 -0500

    added ballpage and ball widgets

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,45 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text('Ask Me Anything'),
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                print('I got clicked');
+              },
+              child: Image.asset('images/ball1.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
